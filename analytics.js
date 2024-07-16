b4fd24ad95db014529e7d92db9219a5d691cb916
Cristian Sclifos <92810054+CSwebD@users.noreply.github.com>
2024-07-16 12:59:40 +0100

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,13 @@
+(function() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E4TX9S1N3H';
+    document.head.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'G-E4TX9S1N3H');
+  })();
